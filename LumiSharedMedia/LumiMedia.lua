9ca179aff94b78cf40f5e51ee6cb6edb7b49088f
lumidtc <88871921+lumidtc@users.noreply.github.com>
2022-04-07 16:07:30 -0400

    Add files via upload

---
+++ LumiSharedMedia/LumiMedia.lua
@@ -0,0 +1,32 @@
+local LSM = LibStub("LibSharedMedia-3.0") 
+
+-- ----- 
+-- StatusBars
+-- -----
+LSM:Register("statusbar", "|cff8246faLumi|r", [[Interface\AddOns\LumiSharedMedia\media\textures\Lumi.tga]])
+-- ----
+--  TEXTURES
+-- ---- 
+LSM:Register("background", "BackLeft", [[Interface\AddOns\LumiSharedMedia\media\textures\BackLeft.tga]])
+LSM:Register("background", "BackMiddle", [[Interface\AddOns\LumiSharedMedia\media\textures\BackMiddle.tga]])
+LSM:Register("background", "BackRight", [[Interface\AddOns\LumiSharedMedia\media\textures\BackRight.tga]])
+LSM:Register("background", "Front", [[Interface\AddOns\LumiSharedMedia\media\textures\Front.tga]])
+LSM:Register("background", "Left", [[Interface\AddOns\LumiSharedMedia\media\textures\Left.tga]])
+LSM:Register("background", "Right", [[Interface\AddOns\LumiSharedMedia\media\textures\Right.tga]])
+LSM:Register("background", "Plus", [[Interface\AddOns\LumiSharedMedia\media\textures\Plus.tga]])
+LSM:Register("background", "Torment1", [[Interface\AddOns\LumiSharedMedia\media\textures\Torment1.tga]])
+LSM:Register("background", "Torment2", [[Interface\AddOns\LumiSharedMedia\media\textures\Torment2.tga]])
+LSM:Register("background", "Torment3", [[Interface\AddOns\LumiSharedMedia\media\textures\Torment3.tga]])
+LSM:Register("background", "Torment4", [[Interface\AddOns\LumiSharedMedia\media\textures\Torment4.tga]])
+LSM:Register("background", "Torment5", [[Interface\AddOns\LumiSharedMedia\media\textures\Torment5.tga]])
+-- -----
+--   FONT
+-- -----
+LSM:Register("font", "|cff8246faITCAvantGardeStdBk|r", [[Interface\Addons\LumiSharedMedia\media\fonts\ITCAvantGardeStdBk.ttf]])
+LSM:Register("font", "|cff8246faITCAvantGardeStdBold|r", [[Interface\Addons\LumiSharedMedia\media\fonts\ITCAvantGardeStdBold.ttf]])
+LSM:Register("font", "|cff8246faITCAvantGardeStdDemi|r", [[Interface\Addons\LumiSharedMedia\media\fonts\ITCAvantGardeStdDemi.ttf]])
+LSM:Register("font", "|cff8246faITCAvantGardeStdMd|r", [[Interface\Addons\LumiSharedMedia\media\fonts\ITCAvantGardeStdMd.ttf]])
+LSM:Register("font", "|cff8246faITCAvantGardeStdXLt|r", [[Interface\Addons\LumiSharedMedia\media\fonts\ITCAvantGardeStdXLt.ttf]])
+-- -----
+--   SOUND
+-- -----
