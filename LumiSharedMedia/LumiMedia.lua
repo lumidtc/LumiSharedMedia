14967112965756f902e785e8c9c74515bb1c7194
lumidtc <88871921+lumidtc@users.noreply.github.com>
2022-04-20 20:42:51 -0400

    Add files via upload

--- LumiSharedMedia/LumiMedia.lua
+++ LumiSharedMedia/LumiMedia.lua
@@ -1,12 +1,15 @@
 local LSM = LibStub("LibSharedMedia-3.0") 
-
--- ----- 
--- StatusBars
--- -----
-LSM:Register("statusbar", "|cff8246faLumi|r", [[Interface\AddOns\LumiSharedMedia\media\textures\Lumi.tga]])
--- ----
---  TEXTURES
--- ---- 
+--
+----
+-----StatusBars
+----
+--
+LSM:Register("statusbar", "|cff820afaLumi|r", [[Interface\AddOns\LumiSharedMedia\media\textures\Lumi.tga]])
+--
+----
+-----Textures
+----
+--
 LSM:Register("background", "BackLeft", [[Interface\AddOns\LumiSharedMedia\media\textures\BackLeft.tga]])
 LSM:Register("background", "BackMiddle", [[Interface\AddOns\LumiSharedMedia\media\textures\BackMiddle.tga]])
 LSM:Register("background", "BackRight", [[Interface\AddOns\LumiSharedMedia\media\textures\BackRight.tga]])
@@ -20,23 +23,97 @@ LSM:Register("background", "Torment3", [[Interface\AddOns\LumiSharedMedia\media\
 LSM:Register("background", "Torment4", [[Interface\AddOns\LumiSharedMedia\media\textures\Torment4.tga]])
 LSM:Register("background", "Torment5", [[Interface\AddOns\LumiSharedMedia\media\textures\Torment5.tga]])
 LSM:Register("background", "Ring", [[Interface\AddOns\LumiSharedMedia\media\textures\Ring.tga]])
-LSM:Register("background", "Ring2", [[Interface\AddOns\LumiSharedMedia\media\textures\Ring2.tga]])
-LSM:Register("background", "Ring3", [[Interface\AddOns\LumiSharedMedia\media\textures\Ring3.tga]])
 LSM:Register("background", "+", [[Interface\AddOns\LumiSharedMedia\media\textures\+.tga]])
--- -----
---   FONT
--- -----
-LSM:Register("font", "|cff8246faITCAvantGardeStdBk|r", [[Interface\Addons\LumiSharedMedia\media\fonts\ITCAvantGardeStdBk.ttf]])
-LSM:Register("font", "|cff8246faITCAvantGardeStdBold|r", [[Interface\Addons\LumiSharedMedia\media\fonts\ITCAvantGardeStdBold.ttf]])
-LSM:Register("font", "|cff8246faITCAvantGardeStdDemi|r", [[Interface\Addons\LumiSharedMedia\media\fonts\ITCAvantGardeStdDemi.ttf]])
-LSM:Register("font", "|cff8246faITCAvantGardeStdMd|r", [[Interface\Addons\LumiSharedMedia\media\fonts\ITCAvantGardeStdMd.ttf]])
-LSM:Register("font", "|cff8246faITCAvantGardeStdXLt|r", [[Interface\Addons\LumiSharedMedia\media\fonts\ITCAvantGardeStdXLt.ttf]])
--- -----
---   SOUND
--- -----
-LSM:Register("sound", "|cFF8246faFront|r", [[Interface\Addons\LumiSharedMedia\media\sounds\Front.mp3]])
-LSM:Register("sound", "|cFF8246faBackMiddle|r", [[Interface\Addons\LumiSharedMedia\media\sounds\BackMiddle.mp3]])
-LSM:Register("sound", "|cFF8246faLeft|r", [[Interface\Addons\LumiSharedMedia\media\sounds\Left.mp3]])
-LSM:Register("sound", "|cFF8246faBackLeft|r", [[Interface\Addons\LumiSharedMedia\media\sounds\BackLeft.mp3]])
-LSM:Register("sound", "|cFF8246faRight|r", [[Interface\Addons\LumiSharedMedia\media\sounds\Right.mp3]])
-LSM:Register("sound", "|cFF8246faBackRight|r", [[Interface\Addons\LumiSharedMedia\media\sounds\BackRight.mp3]])
+--
+--Logo
+--
+LSM:Register("background", "Logo", [[Interface\AddOns\LumiSharedMedia\media\textures\Logo.tga]])
+--
+----
+-----Fonts
+----
+--
+LSM:Register("font", "|cff820afaITCAvantGardeStdBk|r", [[Interface\Addons\LumiSharedMedia\media\fonts\ITCAvantGardeStdBk.ttf]])
+LSM:Register("font", "|cff820afaITCAvantGardeStdBold|r", [[Interface\Addons\LumiSharedMedia\media\fonts\ITCAvantGardeStdBold.ttf]])
+LSM:Register("font", "|cff820afaITCAvantGardeStdDemi|r", [[Interface\Addons\LumiSharedMedia\media\fonts\ITCAvantGardeStdDemi.ttf]])
+LSM:Register("font", "|cff820afaITCAvantGardeStdMd|r", [[Interface\Addons\LumiSharedMedia\media\fonts\ITCAvantGardeStdMd.ttf]])
+LSM:Register("font", "|cff820afaITCAvantGardeStdXLt|r", [[Interface\Addons\LumiSharedMedia\media\fonts\ITCAvantGardeStdXLt.ttf]])
+--
+----
+-----Sounds
+----
+--
+--Positions-#fa0abe
+--
+LSM:Register("sound", "|cFFfa0abeFront|r", [[Interface\Addons\LumiSharedMedia\media\sounds\Front.mp3]])
+LSM:Register("sound", "|cFFfa0abeBackMiddle|r", [[Interface\Addons\LumiSharedMedia\media\sounds\BackMiddle.mp3]])
+LSM:Register("sound", "|cFFfa0abeLeft|r", [[Interface\Addons\LumiSharedMedia\media\sounds\Left.mp3]])
+LSM:Register("sound", "|cFFfa0abeBackLeft|r", [[Interface\Addons\LumiSharedMedia\media\sounds\BackLeft.mp3]])
+LSM:Register("sound", "|cFFfa0abeRight|r", [[Interface\Addons\LumiSharedMedia\media\sounds\Right.mp3]])
+LSM:Register("sound", "|cFFfa0abeBackRight|r", [[Interface\Addons\LumiSharedMedia\media\sounds\BackRight.mp3]])
+--
+--Countdowns-#82fa0a
+--
+LSM:Register("sound", "|cFF82fa0aC3M|r", [[Interface\Addons\LumiSharedMedia\media\sounds\C3M.mp3]])
+LSM:Register("sound", "|cFF82fa0aC3|r", [[Interface\Addons\LumiSharedMedia\media\sounds\C3.mp3]])
+LSM:Register("sound", "|cFF82fa0aC5|r", [[Interface\Addons\LumiSharedMedia\media\sounds\C5.mp3]])
+LSM:Register("sound", "|cFF82fa0aC10              |r", [[Interface\Addons\LumiSharedMedia\media\sounds\C10.mp3]])
+--
+--AbilityCallouts
+--
+--#0a82fa
+--
+LSM:Register("sound", "|cFF0a82faBait|r", [[Interface\Addons\LumiSharedMedia\media\sounds\Bait.mp3]])
+LSM:Register("sound", "|cFF0a82faClear|r", [[Interface\Addons\LumiSharedMedia\media\sounds\Clear.mp3]])
+--
+--#0afa82
+--
+LSM:Register("sound", "|cFF0afa82Pools|r", [[Interface\Addons\LumiSharedMedia\media\sounds\Pools.mp3]])
+--
+--#46fa0a
+--
+LSM:Register("sound", "|cFF46fa0aGrip|r", [[Interface\Addons\LumiSharedMedia\media\sounds\Grip.mp3]])
+LSM:Register("sound", "|cFF46fa0aKnockback|r", [[Interface\Addons\LumiSharedMedia\media\sounds\Knockback.mp3]])
+LSM:Register("sound", "|cFF46fa0aPull|r", [[Interface\Addons\LumiSharedMedia\media\sounds\Pull.mp3]])
+LSM:Register("sound", "|cFF46fa0aPush|r", [[Interface\Addons\LumiSharedMedia\media\sounds\Push.mp3]])
+--
+--#820afa
+--
+LSM:Register("sound", "|cFF820afaDispel|r", [[Interface\Addons\LumiSharedMedia\media\sounds\Dispel.mp3]])
+--
+--#be0afa
+--
+LSM:Register("sound", "|cFFbe0afaSoak|r", [[Interface\Addons\LumiSharedMedia\media\sounds\Soak.mp3]])
+--
+--#fa0a46
+--
+LSM:Register("sound", "|cFFfa0a46Beam|r", [[Interface\Addons\LumiSharedMedia\media\sounds\Beam.mp3]])
+LSM:Register("sound", "|cFFfa0a46Dodge|r", [[Interface\Addons\LumiSharedMedia\media\sounds\Dodge.mp3]])
+--
+--#fa0a82
+--
+LSM:Register("sound", "|cFFfa0a82Hide|r", [[Interface\Addons\LumiSharedMedia\media\sounds\Hide.mp3]])
+LSM:Register("sound", "|cFFfa0a82In|r", [[Interface\Addons\LumiSharedMedia\media\sounds\In.mp3]])
+LSM:Register("sound", "|cFFfa0a82Move|r", [[Interface\Addons\LumiSharedMedia\media\sounds\Move.mp3]])
+LSM:Register("sound", "|cFFfa0a82Out|r", [[Interface\Addons\LumiSharedMedia\media\sounds\Out.mp3]])
+LSM:Register("sound", "|cFFfa0a82Run|r", [[Interface\Addons\LumiSharedMedia\media\sounds\Run.mp3]])
+LSM:Register("sound", "|cFFfa0a82StopMoving|r", [[Interface\Addons\LumiSharedMedia\media\sounds\StopMoving.mp3]])
+--
+--#fa460a
+--
+LSM:Register("sound", "|cFFfa460aCone|r", [[Interface\Addons\LumiSharedMedia\media\sounds\Cone.mp3]])
+LSM:Register("sound", "|cFFfa460aFrontal|r", [[Interface\Addons\LumiSharedMedia\media\sounds\Frontal.mp3]])
+--
+--#fafa0a
+--
+LSM:Register("sound", "|cFFfafa0aSpread|r", [[Interface\Addons\LumiSharedMedia\media\sounds\Spread.mp3]])
+--
+--SoundEffects-#0afabe
+--
+LSM:Register("sound", "|cFF0afabe123B|r", [[Interface\Addons\LumiSharedMedia\media\sounds\123B.mp3]])
+LSM:Register("sound", "|cFF0afabeWarble1|r", [[Interface\Addons\LumiSharedMedia\media\sounds\Warble1.mp3]])
+LSM:Register("sound", "|cFF0afabeWarble2|r", [[Interface\Addons\LumiSharedMedia\media\sounds\Warble2.mp3]])
+LSM:Register("sound", "|cFF0afabeWaterDroplet1|r", [[Interface\Addons\LumiSharedMedia\media\sounds\WaterDroplet1.mp3]])
+
+--unused
+
